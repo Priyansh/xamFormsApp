e3e81b9bd061f1a4f2f601dfc461b087bd1ea60f
Priyansh <priyansh.shah@mail.com>
2018-02-19 19:25:56 -0500

    Added Resources Instagram type App

---
+++ xamFormsApp/xamFormsApp/NavInstaMasterDetailExcercise.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace xamFormsApp
+{
+    public partial class NavInstaMasterDetailExcercise : ContentPage
+    {
+        public NavInstaMasterDetailExcercise()
+        {
+            InitializeComponent();
+        }
+    }
+}
