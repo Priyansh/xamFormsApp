0030f155d7e07233598491c59856ee3b28a2ea86
Priyansh Shah <priyanshshah.bece@hotmail.com>
2018-02-23 10:34:19 -0500

    Revert "Updated with AirBnbSearch case"
    This reverts commit 985466508aa56bb51e8fc29d4555597e1f02fc72.

--- xamFormsApp/xamFormsApp/NavInstaMasterDetailExcercise.xaml.cs
+++ xamFormsApp/xamFormsApp/NavInstaMasterDetailExcercise.xaml.cs
@@ -7,13 +7,11 @@ namespace xamFormsApp
 {
     public partial class NavInstaMasterDetailExcercise : TabbedPage
     {
-        List<AirbnbSearchGroup> SearchGroup;
         public NavInstaMasterDetailExcercise()
         {
             InitializeComponent();
             lstActivityElement.ItemsSource = new ActivityService().GetActivities();
-            var itemSearches = new AirBnbSearchService().GetSearches();
-            populateAirBnbSearch(itemSearches);
+            lstProfileElement.ItemsSource = new ActivityService().GetActivities();
         }
 
         private async void lstActivityElement_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -24,26 +22,6 @@ namespace xamFormsApp
             var userInfo = new UserService().GetUser(getActivity.UserId);
             await Navigation.PushAsync(new NavInstaDetailUserInfo(userInfo));
             lstActivityElement.SelectedItem = null;
-        }
-
-        private void populateAirBnbSearch(IEnumerable<AirbnbSearch> itemSearches)
-        {
-            SearchGroup = new List<AirbnbSearchGroup>
-            {
-                new AirbnbSearchGroup("Recent Searches", itemSearches)
-            };
-            lstAirbnbElement.ItemsSource = SearchGroup;
-        }
-
-        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            var itemSearches = new AirBnbSearchService().GetSearches(e.NewTextValue);
-            populateAirBnbSearch(itemSearches);
-        }
-
-        private void DeleteItem_Clicked(object sender, EventArgs e)
-        {
-
         }
     }
 }
