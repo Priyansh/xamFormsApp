0164ecad9a1f3e2353f0a2a7e31fc96297a3bde5
Priyansh Shah <priyanshshah.bece@hotmail.com>
2018-02-23 10:43:46 -0500

    Updated app with AirBnbService & Search

---
+++ xamFormsApp/xamFormsApp/Models/AirbnbSearchGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xamFormsApp
+{
+    public class AirbnbSearchGroup : ObservableCollection<AirbnbSearch>
+    {
+        public string Title { get; set; }
+        public AirbnbSearchGroup(string Title, IEnumerable<AirbnbSearch> airbnbSearch) : base(airbnbSearch)
+        {
+            this.Title = Title;
+        }
+    }
+}
---
+++ xamFormsApp/xamFormsApp/Service/AirBnbSearchService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xamFormsApp
+{
+    public class AirBnbSearchService
+    {
+        private List<AirbnbSearch> _lstAirBnb = new List<AirbnbSearch>
+        {
+            new AirbnbSearch { Id = 1, Location = "223-289 Kenwood Ave Burlington, ON L7L 4L9", CheckIn = new DateTime(2016,05,10), CheckOut = new DateTime(2016,08,12) },
+            new AirbnbSearch { Id = 2, Location = "67 Dogwood crt, Geogegtown, ON M8B 6P9", CheckIn = new DateTime(2012,02,15), CheckOut = new DateTime(2013,06,28) }
+        };
+        public IEnumerable<AirbnbSearch> GetSearches(string filter = null)
+        {
+            if (string.IsNullOrWhiteSpace(filter))
+                return _lstAirBnb;
+            else
+                return _lstAirBnb.Where(a => a.Location.StartsWith(filter, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
